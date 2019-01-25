2fa69b4c5420aa4f92a6c81948d07af038f40174
hieudevx <hoangtrunghieu10licbn@gmail.com>
2019-01-25 15:54:57 +0700

    chapter 04: convert all class components to function componenst

--- Chapter04/reactshop/src/Products/ProductsPage.tsx
+++ Chapter04/reactshop/src/Products/ProductsPage.tsx
@@ -1,65 +1,102 @@
-import * as React from "react";
+import React, { useState, useEffect } from "react";
 import { IProduct, products } from "./ProductsData";
 import { Link } from "react-router-dom";
 import { RouteComponentProps } from "react-router-dom";
 import "url-search-params-polyfill";
 
-interface IState {
-  products: IProduct[];
-  search: string;
-}
+// interface IState {
+//   products: IProduct[];
+//   search: string;
+// }
 
-class ProductsPage extends React.Component<RouteComponentProps, IState> {
-  public constructor(props: RouteComponentProps) {
-    super(props);
-    this.state = {
-      products: [],
-      search: ""
-    };
-  }
+// class ProductsPage extends React.Component<RouteComponentProps, IState> {
+//   public constructor(props: RouteComponentProps) {
+//     super(props);
+//     this.state = {
+//       products: [],
+//       search: ""
+//     };
+//   }
 
-  public componentDidMount() {
-    this.setState({ products });
-  }
+//   public componentDidMount() {
+//     this.setState({ products });
+//   }
 
-  public static getDerivedStateFromProps(
-    props: RouteComponentProps,
-    state: IState
-  ) {
+//   public static getDerivedStateFromProps(
+//     props: RouteComponentProps,
+//     state: IState
+//   ) {
+//     const searchParams = new URLSearchParams(props.location.search);
+//     const search = searchParams.get("search") || "";
+//     return {
+//       // products: state.products,
+//       ...products,
+//       search
+//     };
+//   }
+
+//   public render() {
+//     return (
+//       <div className="page-container">
+//         <p>Welcome React Shop where you can get all your tools for ReactJS!</p>
+//         <ul className="product-list">
+//           {this.state.products.map(product => {
+//             if (
+//               !this.state.search ||
+//               (this.state.search &&
+//                 product.name
+//                   .toLocaleLowerCase()
+//                   .indexOf(this.state.search.toLowerCase()) > -1)
+//             ) {
+//               return (
+//                 <li key={product.id} className="product-list-item">
+//                   <Link to={`/products/${product.id}`}>{product.name}</Link>
+//                 </li>
+//               );
+//             } else {
+//               return null;
+//             }
+//           })}
+//         </ul>
+//       </div>
+//     );
+//   }
+// }
+
+const ProductsPage: React.FC<RouteComponentProps> = props => {
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
     const searchParams = new URLSearchParams(props.location.search);
     const search = searchParams.get("search") || "";
-    return {
-      products: state.products,
-      search
-    };
-  }
 
-  public render() {
-    return (
-      <div className="page-container">
-        <p>Welcome React Shop where you can getall your tools for ReactJS!</p>
-        <ul className="product-list">
-          {this.state.products.map(product => {
-            if (
-              !this.state.search ||
-              (this.state.search &&
-                product.name
-                  .toLocaleLowerCase()
-                  .indexOf(this.state.search.toLowerCase()) > -1)
-            ) {
-              return (
-                <li key={product.id} className="product-list-item">
-                  <Link to={`/products/${product.id}`}>{product.name}</Link>
-                </li>
-              );
-            } else {
-              return null;
-            }
-          })}
-        </ul>
-      </div>
-    );
-  }
-}
+    setSearch(search);
+    console.log(`search state: ${search}`);
+  }, []);
+
+  return (
+    <div className="page-container">
+      <p>Welcome React Shop where you can get all your tools for ReactJS!</p>
+      <ul className="product-list">
+        {products.map(product => {
+          if (
+            !search ||
+            (search &&
+              product.name.toLocaleLowerCase().indexOf(search.toLowerCase()) >
+                -1)
+          ) {
+            return (
+              <li key={product.id} className="product-list-item">
+                <Link to={`/products/${product.id}`}>{product.name}</Link>
+              </li>
+            );
+          } else {
+            return null;
+          }
+        })}
+      </ul>
+    </div>
+  );
+};
 
 export default ProductsPage;
